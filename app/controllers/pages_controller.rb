346abb8c1414f49ad9580714342b9de0c27b0b38
Andrew Olson <andrew@anolson.cc.vt.edu>
2008-09-18 11:59:10 -0400

    Updated Terms and Privacy Policy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,13 @@ class PagesController < ApplicationController
   skip_before_filter :check_authentication
   def index
   end
+  
   def privacy_policy
   end
+  
   def plans
+  end
+  
+  def terms
   end
 end
