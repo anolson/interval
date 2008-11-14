555f1644474ce70c12b05dff83bb03f287888a0e
Andrew Olson <anolson@gmail.com>
2008-11-13 22:41:24 -0500

    checking a plan is within limits also checks the workout_limit is > 0.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -32,7 +32,7 @@ class ApplicationController < ActionController::Base
   end
   
   def check_within_plan_limits
-    if(@user.workouts.size >= @user.plan.workout_limit)
+    if(@user.plan.workout_limit > 0 && @user.workouts.size > @user.plan.workout_limit)
       render :action => 'limit_reached', :layout => 'application'
     end
   end
