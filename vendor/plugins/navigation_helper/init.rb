7013f2ff568ee7b5772e8aa659bb87c36daffa5a
Andrew Olson <andrew@anolson.cc.vt.edu>
2008-12-03 15:29:31 -0500

    removed navigation_helper

--- vendor/plugins/navigation_helper/init.rb
+++
@@ -1,7 +0,0 @@
-require 'navigation'
-
-ActionView::Base.send :include, RPH::Navigation::InstanceMethods
-ActionController::Base.class_eval do
-  extend RPH::Navigation::ClassMethods
-  class_inheritable_accessor :_current_tab
-end
--- vendor/plugins/navigation_helper/tasks/navigation_helper_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :navigation_helper do
-#   # Task goes here
-# end
