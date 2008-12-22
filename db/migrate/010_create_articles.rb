bbd43ab6a2eb5dd83a98fb05b021dcc05097dfdb
Andrew Olson <andrew@anolson.cc.vt.edu>
2008-12-22 17:10:25 -0500

    articles migrations.

--- db/migrate/010_create_articles.rb
+++ db/migrate/010_create_articles.rb
@@ -4,7 +4,7 @@ class CreateArticles < ActiveRecord::Migration
       t.string :title
       t.text :body         
       t.string :category
-      t.integer :number_of_views
+      t.integer :number_of_views, :default => 0
       t.timestamps
     end
   end
