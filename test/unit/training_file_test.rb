f3b4f0ce954622e61353bd920538793b332815af
Andrew Olson <andrew@anolson.cc.vt.edu>
2009-02-12 16:16:11 -0500

    fixing up tests

--- test/unit/training_file_test.rb
+++ test/unit/training_file_test.rb
@@ -25,7 +25,7 @@ class TrainingFileTest < Test::Unit::TestCase
     
  # end
   
-  def test_create_powertap
+  def test_create_powertap_exported_from_wko
     file = uploaded_file("#{File.expand_path(RAILS_ROOT)}/test/fixtures/powertap_exported_from_wko.csv")
     training_file = TrainingFile.create({:payload => file})
     training_file.save
@@ -34,33 +34,56 @@ class TrainingFileTest < Test::Unit::TestCase
     
     assert_equal ".csv", training_file.file_type
     assert_equal 1, training_file.markers.size
-    #assert_equal 138, training_file.markers.first.avg_power.round
-    #assert_equal 6499, training_file.markers.first.duration_seconds
-    #assert_equal 896, training_file.markers.first.energy
-    #assert_equal 346, training_file.peak_powers.first[:value].round
+
   
   end
+
+  def test_create_powertap
+    file = uploaded_file("#{File.expand_path(RAILS_ROOT)}/test/fixtures/11202008.csv")
+    training_file = TrainingFile.create({:payload => file})
+    training_file.save
+    training_file.parse_file_header()
+    training_file.parse_file_data()
+    
+    assert_equal ".csv", training_file.file_type
+    assert_equal 6, training_file.markers.size
+    assert_equal 138, training_file.markers.first.avg_power.round
+    assert_equal 6499, training_file.markers.first.duration_seconds
+    assert_equal 896, training_file.markers.first.energy
+    assert_equal 346, training_file.peak_powers.first[:value].round
   
-  # def test_create_ibike
-     #file = uploaded_file("#{File.expand_path(RAILS_ROOT)}/test/fixtures/Owen.09142008.csv")
-  #   file = uploaded_file("#{File.expand_path(RAILS_ROOT)}/test/fixtures/iBike.csv")
+  end
+
+  
+   def test_create_ibike
+     file = uploaded_file("#{File.expand_path(RAILS_ROOT)}/test/fixtures/ibike.csv")
      
      #training_file = TrainingFile.create({:payload => file})
      
-  #   training_file = TrainingFile.create({:payload => file})
-  #   training_file.parse_file_header()
-  #   training_file.parse_file_data()
-  #   training_file.save
+     training_file = TrainingFile.create({:payload => file})
+     training_file.parse_file_header()
+     training_file.parse_file_data()
+     training_file.save
      
      
-  #   assert_equal ".csv", training_file.file_type
-     #assert_equal 1, training_file.powermeter_properties.record_interval
-     #assert_equal "09/14/08 16:41:52", training_file.powermeter_properties.date_time.strftime("%m/%d/%y %H:%M:%S")
-     #assert_equal "english", training_file.powermeter_properties.units
-     #assert_equal 31.56, (training_file.data_values.last.distance/1609344).round_with_precision(2)
-     #assert_equal 211, training_file.markers.first.avg_power
-     #assert_equal 25.39, (training_file.markers.first.avg_speed/447).round_with_precision(2)
+     assert_equal ".csv", training_file.file_type
+     assert_equal 1, training_file.powermeter_properties.record_interval
+     assert_equal "09/14/08 16:41:52", training_file.powermeter_properties.date_time.strftime("%m/%d/%y %H:%M:%S")
+     assert_equal "english", training_file.powermeter_properties.units
+     assert_equal 31.56, (training_file.data_values.last.distance/1609344).round_with_precision(2)
+     assert_equal 212, training_file.markers.first.avg_power
+     assert_equal 25.39, (training_file.markers.first.avg_speed/447).round_with_precision(2)
+  end
+
+   def test_create_ibike_mac
+     #file = uploaded_file("#{File.expand_path(RAILS_ROOT)}/test/fixtures/Owen.09142008.csv")
+     file = uploaded_file("#{File.expand_path(RAILS_ROOT)}/test/fixtures/ibike_mac.csv")
+     training_file = TrainingFile.create({:payload => file})
+     training_file.parse_file_header()
+     training_file.parse_file_data()
+     training_file.save
      
-  # end
+     assert_equal ".csv", training_file.file_type
+  end
     
 end
