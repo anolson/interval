e9bb9035740298fd560d4eca649fdd8609c4dbd6
Andrew Olson <andrew@anolson.cc.vt.edu>
2009-12-04 13:57:39 -0500

    updating config

--- db/schema.rb
+++ db/schema.rb
@@ -69,8 +69,6 @@ ActiveRecord::Schema.define(:version => 21) do
     t.float   "intensity_factor"
   end
 
-  add_index "markers", ["workout_id"], :name => "index_markers_on_workout_id"
-
   create_table "peak_powers", :force => true do |t|
     t.integer "duration",   :default => 0
     t.integer "start",      :default => 0
@@ -78,14 +76,12 @@ ActiveRecord::Schema.define(:version => 21) do
     t.integer "workout_id", :default => 0
   end
 
-  add_index "peak_powers", ["workout_id"], :name => "index_peak_powers_on_workout_id"
-
   create_table "plans", :force => true do |t|
     t.string   "name"
     t.text     "description"
     t.integer  "workout_limit"
     t.string   "limit_by"
-    t.integer  "storage_limit"
+    t.integer  "storage_limit", :limit => 8
     t.float    "price"
     t.boolean  "enabled"
     t.boolean  "public"
@@ -127,8 +123,6 @@ ActiveRecord::Schema.define(:version => 21) do
     t.integer  "workout_id"
   end
 
-  add_index "training_files", ["workout_id"], :name => "index_training_files_on_workout_id"
-
   create_table "users", :force => true do |t|
     t.datetime "created_at"
     t.boolean  "disabled",             :default => false
@@ -139,9 +133,8 @@ ActiveRecord::Schema.define(:version => 21) do
     t.boolean  "terms_of_service"
     t.text     "preferences"
     t.text     "email"
-    t.string   "upload_email_address"
-    t.string   "upload_email_secret"
     t.string   "private_sharing_hash"
+    t.string   "upload_email_secret"
   end
 
   create_table "workouts", :force => true do |t|
@@ -155,7 +148,5 @@ ActiveRecord::Schema.define(:version => 21) do
     t.string   "state",        :default => "created"
     t.boolean  "shared",       :default => true
   end
-
-  add_index "workouts", ["user_id"], :name => "index_workouts_on_user_id"
 
 end
