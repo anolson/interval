e1ce594d0db34016e9a7ef10a75a3393649190d3
Andrew Olson <anolson@gmail.com>
2010-03-23 09:47:50 -0400

    Adding seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
+#default admin role
+role = Role.create(:name => 'admin', :description => 'Role for all administrators.')
+
+#default admin user, change the password in production
+user = User.new(
+  :username => 'admin', 
+  :password => '8OkOmke1FK7n', 
+  :password_confirmation => '8OkOmke1FK7n', 
+  :terms_of_service => true, 
+  :email => 'admin@localhost')
+  
+user.roles << role
+user.save
