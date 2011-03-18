fdc06a99c34e0a157f4f572c543b196f1b0e7e32
Andrew Olson <anolson@gmail.com>
2011-03-18 10:51:06 -0400

    Use :active_record_store.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 26) do
+ActiveRecord::Schema.define(:version => 27) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -111,6 +111,16 @@ ActiveRecord::Schema.define(:version => 26) do
   end
 
   add_index "roles_users", ["role_id", "user_id"], :name => "index_roles_users_on_role_id_and_user_id", :unique => true
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "training_files", :force => true do |t|
     t.datetime "created_at"
